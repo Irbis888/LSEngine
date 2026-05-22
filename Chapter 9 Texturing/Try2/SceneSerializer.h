0146f943588b7f1363be0f8e4ca04a8b91892dff
Касик Максим Олегович <maxxka@outlook.com>
2026-05-22 16:39:19 +0300

    Added materials in serialized json

--- Chapter 9 Texturing/Try2/SceneSerializer.h
+++ Chapter 9 Texturing/Try2/SceneSerializer.h
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include "ResourceManager.h"
 #include "World.h"
 
 class SceneSerializer
@@ -9,4 +10,5 @@ class SceneSerializer
 public:
     static void Save(const World& world, const std::string& path);
     static void Load(World& world, const std::string& path);
+    static void Load(World& world, ResourceManager& resources, const std::string& path);
 };
